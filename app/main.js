5574d64f3bebbd7725679d48c61daedef8b1c177
Baibhavos <baibhavkumar303@gmail.com>
2024-08-25 10:55:01 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,11 +1,23 @@
+//  Match Digits
+// function matchPattern(inputLine, pattern) {
+//   if (pattern.length === 1) {
+//     return inputLine.includes(pattern);
+//   } else if (pattern === '\\d') {
+//     return /\d/.test(inputLine);
+//   } else {
+//     throw new Error(`Unhandled pattern ${pattern}`);
+//   }
+// }
+
+//  Match alphanumeric
 function matchPattern(inputLine, pattern) {
-  if (pattern.length === 1) {
-    return inputLine.includes(pattern);
-  } else if (pattern === '\\d') {
-    return /\d/.test(inputLine);
-  } else {
-    throw new Error(`Unhandled pattern ${pattern}`);
-  }
+   if (pattern.length === 1) {
+      return inputLine.includes(pattern);
+   } else if (pattern === "\\w") {
+      return /\w/.test(inputLine);
+   } else {
+      throw new Error(`Unhandled pattern ${pattern}`);
+   }
 }
 
 function main() {
