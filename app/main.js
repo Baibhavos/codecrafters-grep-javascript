f5d618e8aa519fbef917df1bf357ec4e01788ad9
Baibhavos <baibhavkumar303@gmail.com>
2024-08-26 05:59:59 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -23,32 +23,32 @@ function matchPattern(inputLine, pattern) {
       }
       regexPattern = regexPattern.trim();
 
-      const regex = new RegExp(`^${regexPattern}$`, "i");
+      const regex = new RegExp(`${regexPattern}`, "i");
       return regex.test(inputLine);
    } else {
-      const regex = new RegExp(`^${createRegexFromPattern(pattern)}$`, "i");
+      const regex = new RegExp(`${createRegexFromPattern(pattern)}`, "i");
       return regex.test(inputLine);
-  }
+   }
 }
 
 function main() {
-  const pattern = process.argv[3];
-  const inputLine = require("fs").readFileSync(0, "utf-8").trim();
+   const pattern = process.argv[3];
+   const inputLine = require("fs").readFileSync(0, "utf-8").trim();
 
-  if (process.argv[2] !== "-E") {
-    console.log("Expected first argument to be '-E'");
-    process.exit(1);
-  }
+   if (process.argv[2] !== "-E") {
+      console.log("Expected first argument to be '-E'");
+      process.exit(1);
+   }
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  console.log("Logs from your program will appear here");
+   // You can use print statements as follows for debugging, they'll be visible when running tests.
+   console.log("Logs from your program will appear here");
 
-  // Uncomment this block to pass the first stage
-  if (matchPattern(inputLine, pattern)) {
-    process.exit(0);
-  } else {
-    process.exit(1);
-  }
+   // Uncomment this block to pass the first stage
+   if (matchPattern(inputLine, pattern)) {
+      process.exit(0);
+   } else {
+      process.exit(1);
+   }
 }
 
 main();
